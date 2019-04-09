a8a52cdf114375bbed0c90c07a40221f79b7be9b
Liran Sharir <lsharir@gmail.com>
2019-04-09 18:57:41 -0400

    Show healthy icon

--- contentScript.js
+++ contentScript.js
@@ -3,7 +3,10 @@ healthy = ["B008U5OSTQ", "B000QJGLHG"]
 window.setTimeout(function() {
     document.querySelectorAll("div[class^='asin_card__root__']").forEach(el => {
         if (-1 !== healthy.indexOf(el.children[0].href.split("/")[4].split("?")[0])) {
-            el.style.backgroundColor = "green"
+            const img = document.createElement('img')
+            img.src = chrome.extension.getURL("/assets/healthy-seal.png");
+            img.style.cssText = "position: absolute; left: 0; top: 0; margin: 0.5rem; width: 5rem; height: auto;";
+            el.append(img);
         }
     })
 }, 100);
