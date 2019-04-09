a6ba520a416df63aa7ce79e9300dd7dc322b7ea0
Liran Sharir <lsharir@gmail.com>
2019-04-09 19:04:49 -0400

    Fix small bug

--- contentScript.js
+++ contentScript.js
@@ -9,25 +9,27 @@ healthy = yogurt.concat(bread).concat(cereal)
 
 chrome.storage.local.get(['type'], function(result) {
     console.log("User type: " + result.type)
-    switch(result.type) {
-        case TYPE_HIDE:
-            document.querySelectorAll("div[class^='asin_card__root__']").forEach(el => {
-                if (-1 === healthy.indexOf(el.children[0].href.split("/")[4].split("?")[0])) {
-                    el.parentElement.style.display = "none"
-                }
-            })
-            break;
-        case TYPE_RECOMMEND:
-            document.querySelectorAll("div[class^='asin_card__root__']").forEach(el => {
-                if (-1 !== healthy.indexOf(el.children[0].href.split("/")[4].split("?")[0])) {
-                    const img = document.createElement('img')
-                    img.src = chrome.extension.getURL("/assets/healthy-seal.png");
-                    img.style.cssText = "position: absolute; left: 0; top: 0; margin: 0.5rem; width: 5rem; height: auto;";
-                    el.append(img);
-                }
-            })
-            break;
-        case TYPE_NOTHING:
-          // code block
-      }
+    window.setTimeout(function() {
+        switch(result.type) {
+            case TYPE_HIDE:
+                document.querySelectorAll("div[class^='asin_card__root__']").forEach(el => {
+                    if (-1 === healthy.indexOf(el.children[0].href.split("/")[4].split("?")[0])) {
+                        el.parentElement.style.display = "none"
+                    }
+                })
+                break;
+            case TYPE_RECOMMEND:
+                document.querySelectorAll("div[class^='asin_card__root__']").forEach(el => {
+                    if (-1 !== healthy.indexOf(el.children[0].href.split("/")[4].split("?")[0])) {
+                        const img = document.createElement('img')
+                        img.src = chrome.extension.getURL("/assets/healthy-seal.png");
+                        img.style.cssText = "position: absolute; left: 0; top: 0; margin: 0.5rem; width: 5rem; height: auto;";
+                        el.append(img);
+                    }
+                })
+                break;
+            case TYPE_NOTHING:
+              // nothing
+          }
+    }, 100);
 });
