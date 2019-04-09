f78ef45eb33fe6057aa5a9fc549128289744ed8e
Vicente Rotman <vrotmanh@gmail.com>
2019-04-09 18:30:49 -0400

    Set Status and switch between them

--- background.js
+++ background.js
@@ -1,3 +1,8 @@
 chrome.runtime.onInstalled.addListener(function() {
-  console.log("Extension is here!")
+  chrome.storage.local.get(['type'], function(result) {
+    if(!result.type){
+      const type = Math.floor(Math.random() * 3);
+      chrome.storage.local.set({type: type}, function() {});
+    }
+  });
 });
--- contentScript.js
+++ contentScript.js
@@ -1,9 +1,28 @@
-healthy = ["B008U5OSTQ", "B000QJGLHG"]
+TYPE_HIDE = 0
+TYPE_RECOMMEND = 1
+TYPE_NOTHING = 2
 
-window.setTimeout(function() {
-    document.querySelectorAll("div[class^='asin_card__root__']").forEach(el => {
-        if (-1 !== healthy.indexOf(el.children[0].href.split("/")[4].split("?")[0])) {
-            el.style.backgroundColor = "green"
-        }
-    })
-}, 100);
+yogurt = ["B000VRC1NG", "B00FZHEGGW", "B008U5OSTQ", "B002M3ZBPW"]
+bread = ["B001F79MMY", "B074H5B5XG", "B000LR4XJ6", "B004A94260"]
+cereal = ["B005H12NJS", "B01763CJY8", "B0014CSH7G", "B000VK6558"]
+healthy = yogurt.concat(bread).concat(cereal)
+
+chrome.storage.local.get(['type'], function(result) {
+    console.log("User type: " + result.type)
+    switch(result.type) {
+        case TYPE_HIDE:
+          // code block
+          break;
+        case TYPE_RECOMMEND:
+            window.setTimeout(function() {
+                document.querySelectorAll("div[class^='asin_card__root__']").forEach(el => {
+                    if (-1 !== healthy.indexOf(el.children[0].href.split("/")[4].split("?")[0])) {
+                        el.style.backgroundColor = "green"
+                    }
+                })
+            }, 100);
+          break;
+        case TYPE_NOTHING:
+          // code block
+      }
+});
